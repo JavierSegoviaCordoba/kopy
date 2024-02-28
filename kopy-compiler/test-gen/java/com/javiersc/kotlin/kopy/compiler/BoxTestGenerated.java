39d07b10f58ab321ec9e88ba9038ede55201863b
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2024-02-29 00:34:15 +0100

    Update dependencies

--- kopy-compiler/test-gen/java/com/javiersc/kotlin/kopy/compiler/BoxTestGenerated.java
+++ kopy-compiler/test-gen/java/com/javiersc/kotlin/kopy/compiler/BoxTestGenerated.java
@@ -17,74 +17,74 @@ import java.util.regex.Pattern;
 @TestMetadata("test-data/box")
 @TestDataPath("$PROJECT_ROOT")
 public class BoxTestGenerated extends AbstractBoxTest {
+  @Test
+  public void testAllFilesPresentInBox() {
+    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
+  }
+
+  @Test
+  @TestMetadata("nest-copy-set.kt")
+  public void testNest_copy_set() {
+    runTest("test-data/box/nest-copy-set.kt");
+  }
+
+  @Test
+  @TestMetadata("no-nest-copy-set.kt")
+  public void testNo_nest_copy_set() {
+    runTest("test-data/box/no-nest-copy-set.kt");
+  }
+
+  @Nested
+  @TestMetadata("test-data/box/edge")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Edge {
     @Test
-    public void testAllFilesPresentInBox() throws Exception {
-        KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
+    public void testAllFilesPresentInEdge() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box/edge"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
     }
 
     @Test
-    @TestMetadata("nest-copy-set.kt")
-    public void testNest_copy_set() throws Exception {
-        runTest("test-data/box/nest-copy-set.kt");
+    @TestMetadata("simple-1.kt")
+    public void testSimple_1() {
+      runTest("test-data/box/edge/simple-1.kt");
+    }
+  }
+
+  @Nested
+  @TestMetadata("test-data/box/nested-copy")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Nested_copy {
+    @Test
+    public void testAllFilesPresentInNested_copy() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box/nested-copy"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
     }
 
     @Test
-    @TestMetadata("no-nest-copy-set.kt")
-    public void testNo_nest_copy_set() throws Exception {
-        runTest("test-data/box/no-nest-copy-set.kt");
+    @TestMetadata("simple-1.kt")
+    public void testSimple_1() {
+      runTest("test-data/box/nested-copy/simple-1.kt");
+    }
+  }
+
+  @Nested
+  @TestMetadata("test-data/box/repeated-properties")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Repeated_properties {
+    @Test
+    public void testAllFilesPresentInRepeated_properties() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box/repeated-properties"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
     }
 
-    @Nested
-    @TestMetadata("test-data/box/edge")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Edge {
-        @Test
-        public void testAllFilesPresentInEdge() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box/edge"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
-        }
-
-        @Test
-        @TestMetadata("simple-1.kt")
-        public void testSimple_1() throws Exception {
-            runTest("test-data/box/edge/simple-1.kt");
-        }
+    @Test
+    @TestMetadata("complex-1.kt")
+    public void testComplex_1() {
+      runTest("test-data/box/repeated-properties/complex-1.kt");
     }
 
-    @Nested
-    @TestMetadata("test-data/box/nested-copy")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Nested_copy {
-        @Test
-        public void testAllFilesPresentInNested_copy() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box/nested-copy"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
-        }
-
-        @Test
-        @TestMetadata("simple-1.kt")
-        public void testSimple_1() throws Exception {
-            runTest("test-data/box/nested-copy/simple-1.kt");
-        }
+    @Test
+    @TestMetadata("complex-2.kt")
+    public void testComplex_2() {
+      runTest("test-data/box/repeated-properties/complex-2.kt");
     }
-
-    @Nested
-    @TestMetadata("test-data/box/repeated-properties")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Repeated_properties {
-        @Test
-        public void testAllFilesPresentInRepeated_properties() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/box/repeated-properties"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
-        }
-
-        @Test
-        @TestMetadata("complex-1.kt")
-        public void testComplex_1() throws Exception {
-            runTest("test-data/box/repeated-properties/complex-1.kt");
-        }
-
-        @Test
-        @TestMetadata("complex-2.kt")
-        public void testComplex_2() throws Exception {
-            runTest("test-data/box/repeated-properties/complex-2.kt");
-        }
-    }
+  }
 }
--- kopy-compiler/test-gen/java/com/javiersc/kotlin/kopy/compiler/DiagnosticTestGenerated.java
+++ kopy-compiler/test-gen/java/com/javiersc/kotlin/kopy/compiler/DiagnosticTestGenerated.java
@@ -16,154 +16,154 @@ import java.util.regex.Pattern;
 @TestMetadata("test-data/diagnostics")
 @TestDataPath("$PROJECT_ROOT")
 public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
+  @Test
+  public void testAllFilesPresentInDiagnostics() {
+    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics"), Pattern.compile("^(.+)\\.kt$"), null, true);
+  }
+
+  @Nested
+  @TestMetadata("test-data/diagnostics/edge")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Edge {
     @Test
-    public void testAllFilesPresentInDiagnostics() throws Exception {
-        KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics"), Pattern.compile("^(.+)\\.kt$"), null, true);
+    public void testAllFilesPresentInEdge() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/edge"), Pattern.compile("^(.+)\\.kt$"), null, true);
     }
 
-    @Nested
-    @TestMetadata("test-data/diagnostics/edge")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Edge {
-        @Test
-        public void testAllFilesPresentInEdge() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/edge"), Pattern.compile("^(.+)\\.kt$"), null, true);
-        }
-
-        @Test
-        @TestMetadata("no-copy-scope.kt")
-        public void testNo_copy_scope() throws Exception {
-            runTest("test-data/diagnostics/edge/no-copy-scope.kt");
-        }
-
-        @Test
-        @TestMetadata("no-immediate-copy-scope.kt")
-        public void testNo_immediate_copy_scope() throws Exception {
-            runTest("test-data/diagnostics/edge/no-immediate-copy-scope.kt");
-        }
-
-        @Test
-        @TestMetadata("no-nest-copy-set.kt")
-        public void testNo_nest_copy_set() throws Exception {
-            runTest("test-data/diagnostics/edge/no-nest-copy-set.kt");
-        }
+    @Test
+    @TestMetadata("no-copy-scope.kt")
+    public void testNo_copy_scope() {
+      runTest("test-data/diagnostics/edge/no-copy-scope.kt");
     }
 
-    @Nested
-    @TestMetadata("test-data/diagnostics/invalid-call-chain")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Invalid_call_chain {
-        @Test
-        public void testAllFilesPresentInInvalid_call_chain() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/invalid-call-chain"), Pattern.compile("^(.+)\\.kt$"), null, true);
-        }
-
-        @Test
-        @TestMetadata("complex-1.kt")
-        public void testComplex_1() throws Exception {
-            runTest("test-data/diagnostics/invalid-call-chain/complex-1.kt");
-        }
+    @Test
+    @TestMetadata("no-immediate-copy-scope.kt")
+    public void testNo_immediate_copy_scope() {
+      runTest("test-data/diagnostics/edge/no-immediate-copy-scope.kt");
     }
 
-    @Nested
-    @TestMetadata("test-data/diagnostics/missing-data-class")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Missing_data_class {
-        @Test
-        public void testAllFilesPresentInMissing_data_class() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/missing-data-class"), Pattern.compile("^(.+)\\.kt$"), null, true);
-        }
+    @Test
+    @TestMetadata("no-nest-copy-set.kt")
+    public void testNo_nest_copy_set() {
+      runTest("test-data/diagnostics/edge/no-nest-copy-set.kt");
+    }
+  }
 
-        @Test
-        @TestMetadata("complex-1.kt")
-        public void testComplex_1() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/complex-1.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-1.kt")
-        public void testSimple_1() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-1.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-2.kt")
-        public void testSimple_2() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-2.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-3.kt")
-        public void testSimple_3() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-3.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-4.kt")
-        public void testSimple_4() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-4.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-5.kt")
-        public void testSimple_5() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-5.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-6.kt")
-        public void testSimple_6() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-6.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-7.kt")
-        public void testSimple_7() throws Exception {
-            runTest("test-data/diagnostics/missing-data-class/simple-7.kt");
-        }
+  @Nested
+  @TestMetadata("test-data/diagnostics/invalid-call-chain")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Invalid_call_chain {
+    @Test
+    public void testAllFilesPresentInInvalid_call_chain() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/invalid-call-chain"), Pattern.compile("^(.+)\\.kt$"), null, true);
     }
 
-    @Nested
-    @TestMetadata("test-data/diagnostics/non-data-class")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Non_data_class {
-        @Test
-        public void testAllFilesPresentInNon_data_class() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/non-data-class"), Pattern.compile("^(.+)\\.kt$"), null, true);
-        }
+    @Test
+    @TestMetadata("complex-1.kt")
+    public void testComplex_1() {
+      runTest("test-data/diagnostics/invalid-call-chain/complex-1.kt");
+    }
+  }
 
-        @Test
-        @TestMetadata("simple-1.kt")
-        public void testSimple_1() throws Exception {
-            runTest("test-data/diagnostics/non-data-class/simple-1.kt");
-        }
+  @Nested
+  @TestMetadata("test-data/diagnostics/missing-data-class")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Missing_data_class {
+    @Test
+    public void testAllFilesPresentInMissing_data_class() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/missing-data-class"), Pattern.compile("^(.+)\\.kt$"), null, true);
     }
 
-    @Nested
-    @TestMetadata("test-data/diagnostics/valid")
-    @TestDataPath("$PROJECT_ROOT")
-    public class Valid {
-        @Test
-        public void testAllFilesPresentInValid() throws Exception {
-            KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/valid"), Pattern.compile("^(.+)\\.kt$"), null, true);
-        }
-
-        @Test
-        @TestMetadata("complex-1.kt")
-        public void testComplex_1() throws Exception {
-            runTest("test-data/diagnostics/valid/complex-1.kt");
-        }
-
-        @Test
-        @TestMetadata("nested-copy-simple-1.kt")
-        public void testNested_copy_simple_1() throws Exception {
-            runTest("test-data/diagnostics/valid/nested-copy-simple-1.kt");
-        }
-
-        @Test
-        @TestMetadata("simple-1.kt")
-        public void testSimple_1() throws Exception {
-            runTest("test-data/diagnostics/valid/simple-1.kt");
-        }
+    @Test
+    @TestMetadata("complex-1.kt")
+    public void testComplex_1() {
+      runTest("test-data/diagnostics/missing-data-class/complex-1.kt");
     }
+
+    @Test
+    @TestMetadata("simple-1.kt")
+    public void testSimple_1() {
+      runTest("test-data/diagnostics/missing-data-class/simple-1.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-2.kt")
+    public void testSimple_2() {
+      runTest("test-data/diagnostics/missing-data-class/simple-2.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-3.kt")
+    public void testSimple_3() {
+      runTest("test-data/diagnostics/missing-data-class/simple-3.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-4.kt")
+    public void testSimple_4() {
+      runTest("test-data/diagnostics/missing-data-class/simple-4.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-5.kt")
+    public void testSimple_5() {
+      runTest("test-data/diagnostics/missing-data-class/simple-5.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-6.kt")
+    public void testSimple_6() {
+      runTest("test-data/diagnostics/missing-data-class/simple-6.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-7.kt")
+    public void testSimple_7() {
+      runTest("test-data/diagnostics/missing-data-class/simple-7.kt");
+    }
+  }
+
+  @Nested
+  @TestMetadata("test-data/diagnostics/non-data-class")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Non_data_class {
+    @Test
+    public void testAllFilesPresentInNon_data_class() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/non-data-class"), Pattern.compile("^(.+)\\.kt$"), null, true);
+    }
+
+    @Test
+    @TestMetadata("simple-1.kt")
+    public void testSimple_1() {
+      runTest("test-data/diagnostics/non-data-class/simple-1.kt");
+    }
+  }
+
+  @Nested
+  @TestMetadata("test-data/diagnostics/valid")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Valid {
+    @Test
+    public void testAllFilesPresentInValid() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("test-data/diagnostics/valid"), Pattern.compile("^(.+)\\.kt$"), null, true);
+    }
+
+    @Test
+    @TestMetadata("complex-1.kt")
+    public void testComplex_1() {
+      runTest("test-data/diagnostics/valid/complex-1.kt");
+    }
+
+    @Test
+    @TestMetadata("nested-copy-simple-1.kt")
+    public void testNested_copy_simple_1() {
+      runTest("test-data/diagnostics/valid/nested-copy-simple-1.kt");
+    }
+
+    @Test
+    @TestMetadata("simple-1.kt")
+    public void testSimple_1() {
+      runTest("test-data/diagnostics/valid/simple-1.kt");
+    }
+  }
 }
