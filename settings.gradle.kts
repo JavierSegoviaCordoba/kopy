6fafca7c67d95becd70bb7e8ebea2176cb094c30
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2024-04-14 14:30:45 +0200

    Disable JetBrains and Snapshots repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,9 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         google()
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+        // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        // maven("https://oss.sonatype.org/content/repositories/snapshots")
         // mavenLocal()
     }
 
@@ -29,9 +29,9 @@ dependencyResolutionManagement {
         gradlePluginPortal()
         mavenCentral()
         google()
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        sonatypeSnapshot()
+        // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+        // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        // sonatypeSnapshot()
         // mavenLocal()
     }
 }
@@ -42,15 +42,12 @@ val hubdleCatalogVersion: String =
     catalogFile.first { it.contains("hubdleCatalog =") }.split("\"")[1]
 val kotlinCompilerExtensionsVersion: String =
     catalogFile.first { it.contains("javiersc-kotlin-compiler-extensions =") }.split("\"")[1]
-val kotlinStdlibAndTestVersion: String =
-    catalogFile.first { it.contains("javiersc-kotlin =") }.split("\"")[1]
 
 hubdleSettings {
     catalog { //
         version(hubdleCatalogVersion)
         replaceVersion(
             "javiersc-kotlin-compiler-extensions" to kotlinCompilerExtensionsVersion,
-            "javiersc-kotlin" to kotlinStdlibAndTestVersion,
         )
     }
 }
