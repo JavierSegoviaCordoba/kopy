fcfcc7c5280570748b0e8032b5039bf7a5302813
Javier Segovia Córdoba <javier@segoviacordoba.com>
2025-02-19 00:58:54 +0100

    Improve KopyGradlePluginIntegrationTest coverage

--- kopy-gradle-plugin/testIntegration/kotlin/com/javiersc/kotlin/kopy/gradle/plugin/KopyGradlePluginIntegrationTest.kt
+++ kopy-gradle-plugin/testIntegration/kotlin/com/javiersc/kotlin/kopy/gradle/plugin/KopyGradlePluginIntegrationTest.kt
@@ -1,9 +1,16 @@
 package com.javiersc.kotlin.kopy.gradle.plugin
 
 import com.javiersc.gradle.project.test.extensions.GradleProjectTest
+import com.javiersc.kotlin.kopy.args.KopyFunctions
+import com.javiersc.kotlin.kopy.args.KopyVisibility
 import com.javiersc.kotlin.kopy.compiler.KopyCompilerProjectData
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
 import org.junit.jupiter.api.Test
 
 internal class KopyGradlePluginIntegrationTest : GradleProjectTest() {
@@ -51,12 +58,28 @@ internal class KopyGradlePluginIntegrationTest : GradleProjectTest() {
             extensions.findByName("kopy").shouldNotBeNull()
             extensions.findByType(KopyExtension::class.java).shouldNotBeNull()
             val kopy: KopyGradlePlugin = plugins.getPlugin(KopyGradlePlugin::class.java)
+            val kotlin: KotlinMultiplatformExtension =
+                extensions.getByType(KotlinMultiplatformExtension::class)
+            configure<KotlinMultiplatformExtension> { jvm() }
             kopy.getCompilerPluginId().shouldBe("com.javiersc.kotlin.kopy-compiler")
             kopy.getPluginArtifact().apply {
                 groupId shouldBe "com.javiersc.kotlin"
                 artifactId shouldBe "kopy-compiler"
                 version shouldBe KopyCompilerProjectData.Version
             }
+            val jvmCompilation: KotlinCompilation<*> =
+                kotlin.targets.first { it.name.contains("jvm") }.compilations.first()
+            kopy.isApplicable(jvmCompilation).shouldBeTrue()
+            kopy
+                .applyToCompilation(jvmCompilation)
+                .get()
+                .map { it.key to it.value }
+                .shouldBe(
+                    listOf(
+                        KopyFunctions.NAME to KopyFunctions.All.value,
+                        KopyVisibility.NAME to KopyVisibility.Auto.value,
+                    )
+                )
         }
     }
 }
