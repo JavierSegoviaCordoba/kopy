2ca53aa3f8d379b9215bf6ebf54dc5f5b8e975d7
Javier Segovia Córdoba <javier@segoviacordoba.com>
2025-02-23 14:13:59 +0100

    Disable some repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,9 @@ pluginManagement {
         mavenCentral()
         google()
         // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
-         mavenLocal()
+        // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        // maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+        // mavenLocal()
     }
 
     plugins { //
@@ -33,9 +33,9 @@ dependencyResolutionManagement {
         mavenCentral()
         google()
         // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        sonatypeSnapshot()
-         mavenLocal()
+        // maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        // sonatypeSnapshot()
+        // mavenLocal()
     }
 }
 
